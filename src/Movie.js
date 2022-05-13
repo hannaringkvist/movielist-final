70c02539a46f390f6d9ab45f6dbb4c0cbd0784b9
Hanna Ringkvist <ringkvist.hanna@gmail.com>
2022-05-13 22:13:55 +0200

    Projektet i full funktion.

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+const mystyle = {
+  height: "60px",
+  width: "60px"
+};
+
 function amountOfStars(grade){
   var text = [];
   for (var i = 0; i < grade; i++){
-    text.push(<img key={i} src="/images/star.png" alt="Star" className="w-1 p-3"/>);
+    text.push(<img key={i} src="/images/star.png" alt="Star" className="w-1 p-3" style={mystyle}/>);
   }
   return text;
 }
@@ -13,7 +18,7 @@ export default function Movie(props) {
     <li key={props.item.id} className="list-group-item my-2">
       { props.item.title}
       {amountOfStars(props.item.grade)}
-      <img src="/images/delete.png" alt="Delete movie" onClick={() => props.deleteItem(props.item.id)}/>
+      <img src="/images/delete.png" alt="Delete movie" className="w-1 p-3 float-end" style={mystyle} onClick={() => props.deleteItem(props.item.id)}/>
     </li>
   )
 
